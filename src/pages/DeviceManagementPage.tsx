8db839eb4c5fa1654814c3dfcf933a19c33c49bb
felippebutland <butlandfelippe@gmail.com>
2025-10-18 15:00:40 -0300

    fix: adjust

--- src/pages/DeviceManagementPage.tsx
+++ src/pages/DeviceManagementPage.tsx
@@ -41,7 +41,7 @@ interface Device {
     applicableDamageTypes: ApplicableDamageType[];
 }
 
-const API_BASE_URL = 'http://localhost:3737/api/v1';
+const API_BASE_URL = 'https://api-device-evaluation.domusduo.space/api/v1';
 
 export function DeviceManagementPage() {
     const [devices, setDevices] = useState<Device[]>([]);
